24236eb9b6ccc707065b09cddccb4032de331c37
Ibrohimjon-Kurbanov <kurbanovolddev@gmail.com>
2024-09-30 00:34:49 +0200

    4.5.vazifa

---
+++ js/index.js
@@ -0,0 +1,381 @@
+// IF 1-MASALA
+// let num = +prompt("Sonni kiriting:");
+
+// if (num > 0) {
+//   num++;
+// }
+// console.log(num);
+
+// IF 2-MASALA
+// let num = +prompt("Sonni kiriting:");
+
+// if (num > 0) {
+//   num = num + 1;
+// } else {
+//   num = num - 2;
+// }
+
+// console.log(num);
+
+// IF 3-MASALA
+// let num  = +prompt("Sonni kiriting:")
+
+// if ( num > 0) {
+//   num = num + 1;
+// } else if (num == 0) {
+//     num = 10;
+// }else{
+//     num = num - 2;
+// }
+
+// console.log(num);
+
+// IF 4-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// let num3 = +prompt("3-sonni kiriting:");
+
+// let counter = 0;
+
+// if (num1 > 0) {
+//   counter = counter + 1;
+// }
+
+// if (num2 > 0) {
+//   counter = counter + 1;
+// }
+
+// if (num3 > 0) {
+//   counter = counter + 1;
+// }
+
+// console.log(counter);
+
+// IF 5-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// let num3 = +prompt("3-sonni kiriting:");
+
+// let counter1 = 0;
+// let counter2 = 0;
+
+// if (num1 > 0) {
+//   counter1++;
+// } else {
+//   counter2++;
+// }
+
+// if (num2 > 0) {
+//   counter1++;
+// } else {
+//   counter2++;
+// }
+
+// if (num3 > 0) {
+//   counter1++;
+// } else {
+//   counter2++;
+// }
+
+// console.log("Musbat: " + counter1 + "\n" + "Manfiy: " + counter2);
+
+// IF 6-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+
+// let max = 0;
+
+// if (num1 > num2) {
+//     max = num1;
+// }else{
+//     max = num2;
+// }
+
+// console.log(max);
+
+// IF 7-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+
+// let max = 0;
+// let min = 0;
+
+// if (num1 > num2) {
+//   max = num1;
+//   min = num2;
+// } else {
+//   max = num1;
+//   min = num2;
+// }
+// console.log(min + "\n" + max);
+
+// IF 8-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+
+// let max = 0;
+// let min = 0;
+
+// if (num1 > num2) {
+//   max = num1;
+//   min = num2;
+// } else {
+//   max = num2;
+//   min = num1;
+// }
+
+// console.log(max + "\n" + min);
+
+// IF 9-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// let res = "";
+
+// if (num1 > num2) {
+//   res = num1;
+//   num1 = num2;
+//   num2 = res;
+// }
+
+// console.log(num1 + "\n" + num2);
+
+// IF 10-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+
+// let res = 0;
+
+// if (num1 == num2) {
+//     res = num1 + num2;
+// }else{
+//     res = 0;
+// }
+
+// console.log(res);
+
+// IF 11-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+
+// let max = 0;
+
+// if (num1 > num2 ) {
+//     max = num1;
+// }else if (num1 < num2) {
+//     max = num2;
+// }else{
+//     max = 0;
+// }
+
+// console.log(max);
+
+// IF 12-MASALA
+
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// let num3 = +prompt("3-sonni kiriting:");
+// let min = 0;
+
+// if (num1 > num2) {
+//   min = num2;
+// } else {
+//   min = num1;
+// }
+
+// if (num2 > num3) {
+//   min = num3;
+// }
+
+// console.log(min);
+
+// IF 13-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// let num3 = +prompt("3-sonni kiriting:");
+// let max = "";
+
+// if (num1 > num2) {
+//   max = num1;
+// } else if (num2 < num3) {
+//   max = num2;
+// } else if (num3 < num2) {
+//   max = num3;
+// }
+// console.log(max);
+
+// IF 14-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// let num3 = +prompt("3-sonni kiriting:");
+// let max = "";
+// let mid = "";
+// let min = "";
+
+// if (num1 <= num2 && num1 <= num3) {
+//   min = num1;
+//   if (num2 <= num3) {
+//     mid = num2;
+//     max = num3;
+//   } else {
+//     mid = num3;
+//     max = num2;
+//   }
+// } else if (num2 <= num1 && num2 <= num3) {
+//   min = num2;
+//   if (num1 <= num3) {
+//     mid = num1;
+//     max = num3;
+//   } else {
+//     mid = num3;
+//     max = num1;
+//   }
+// } else {
+//   min = num3;
+//   if (num1 <= num2) {
+//     mid = num1;
+//     max = num2;
+//   } else {
+//     mid = num2;
+//     max = num1;
+//   }
+// }
+// console.log(min, mid, max);
+
+// IF 15-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// let num3 = +prompt("3-sonni kiriting:");
+
+// let numAB = num1 + num2;
+// let numAC = num1 + num3;
+// let numBC = num2 + num3;
+
+// if (numAB >= numAC && numAB >= numBC) {
+//   console.log(num1, num2);
+// } else if (numAC >= numAB && numAC >= numBC) {
+//   console.log(num1, num3);
+// } else {
+//   console.log(num2, num3);
+// }
+
+// BOOLEAN 1-MASALA
+// let num = +prompt("Sonni kiritng");
+
+// if (num > 0) {
+//   console.log("Musbat son");
+// } else {
+//   console.log("Manfiy son");
+// }
+
+// BOOLEAN 2-MASALA
+// let num1 = +prompt("Sonni kiritng");
+
+// if (num1 % 2 == 1) {
+//   console.log("Toq son");
+// } else {
+//   console.log("Toq son emas");
+// }
+
+// BOOLEAN 3-MASALA
+// let num2 = +prompt("Sonni kiritng");
+
+// if (num2 % 2 == 0) {
+//   console.log("Just son");
+// } else {
+//   console.log("Juft son emas");
+// }
+
+// BOOLEAN 4-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// if (num1 > 2 && num2 <= 3) {
+//   console.log("Jumla tog'ri");
+// } else {
+//   console.log("Jumla notog'ri");
+// }
+
+// BOOLEAN 5-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// if (num1 >= 0 || num2 < -2) {
+//   console.log("Jumla tog'ri");
+// } else {
+//   console.log("Jumla notog'ri");
+// }
+
+// BOOLEAN 6-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// let num3 = +prompt("3-sonni kiriting:");
+
+// if (num1 <= num2 && num2 <= num3) {
+//   console.log("Jumla tog'ri");
+// } else {
+//   console.log("Jumla notog'ri");
+// }
+
+// BOOLEAN 7-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// let num3 = +prompt("3-sonni kiriting:");
+
+// if ((num2 > num1 && num2 < num3) || (num2 > num3 && num2 < num1)) {
+//   console.log("B soni A va C orasida yotadi");
+// } else {
+//   console.log("B soni A va C orasida yotmaydi");
+// }
+
+// BOOLEAN 8-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+
+// if (num1 % 2 == 1 && num2 % 2 == 1) {
+//   console.log("Toq son");
+// } else {
+//   console.log("Toq son emas");
+// }
+
+// BOOLEAN 9-MASALA
+
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+
+// if (num1 % 2 == 1 || num2 % 2 == 1) {
+//   console.log("Toq son");
+// } else {
+//   console.log("Toq son emas");
+// }
+
+// BOOLEAN 10-MASALA
+
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+
+// if ((num1 % 2 == 1 && num2 % 2 == 0) || (num1 % 2 == 0 && num2 % 2 == 1)) {
+//   console.log("Bittasi toq son");
+// } else {
+//   console.log("Toq son yo'q");
+// }
+
+// ALISHER MASALA
+
+// let usd1 = 500;
+// let usd2 = 250;
+// let eur = 120;
+
+// let usd_to_uzs = 11000.34;
+// let eur_to_uzs = 12354.03;
+
+// let usdToUzs1 = usd1 * usd_to_uzs;
+// let usdToUzs2 = usd2 * usd_to_uzs;
+// let eurToUzs = eur * eur_to_uzs;
+
+// let totalprice = usdToUzs1 + usdToUzs2 + eurToUzs;
+
+// let allMoney = +prompt("Alisherni pulini kiriting: (so'mda)");
+
+// if (allMoney >= totalprice) {
+//   console.log("Oq yo'l, Alisher!");
+// } else {
+//   console.log("Alisher ozgina sabr qilish kerak bo'lar ekan");
+// }
